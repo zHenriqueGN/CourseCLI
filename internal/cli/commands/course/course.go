7a655b48c8ecde80e0ec7c77dd6d55fd4f53416a
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-11-25 10:39:31 -0300

    feat: add create course command

--- internal/cli/commands/course/course.go
+++ internal/cli/commands/course/course.go
@@ -2,14 +2,17 @@ package course
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/zHenriqueGN/CourseCLI/internal/cli/commands/course/commands"
 	"github.com/zHenriqueGN/CourseCLI/internal/database"
 )
 
 func NewCourseCmd(courseDB *database.CourseDB) *cobra.Command {
+	createCmd := commands.NewCreateCmd(courseDB)
 
 	var courseCmd cobra.Command
 	courseCmd.Use = "course"
 	courseCmd.Short = "course commands"
+	courseCmd.AddCommand(createCmd)
 
 	return &courseCmd
 }
--- internal/cli/root.go
+++ internal/cli/root.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
 	"github.com/zHenriqueGN/CourseCLI/internal/cli/commands/category"
+	"github.com/zHenriqueGN/CourseCLI/internal/cli/commands/course"
 	"github.com/zHenriqueGN/CourseCLI/internal/database"
 )
 
@@ -15,13 +16,18 @@ func Execute() {
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
+
 	categoryDB := database.NewCategory(db)
+	courseDB := database.NewCourse(db)
+
 	categoryCmd := category.NewCategoryCmd(categoryDB)
+	courseCmd := course.NewCourseCmd(courseDB)
 
 	var rootCmd cobra.Command
 	rootCmd.Use = "course-cli"
 	rootCmd.Short = "course-cli is a CLI for managing courses"
-	rootCmd.AddCommand(categoryCmd)
+	rootCmd.AddCommand(categoryCmd, courseCmd)
 
 	err = rootCmd.Execute()
 	if err != nil {
